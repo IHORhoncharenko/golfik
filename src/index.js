0f7a0bbb63d23a11bfed525064be3e4a516d1b5f
Igor Goncharenko <gon4arenko.igor@gmail.com>
2022-04-08 21:57:06 +0300

    инициализировал проэкт в Webpack

---
+++ src/index.js
@@ -0,0 +1,7 @@
+import "../style.css";
+// import companyLogo from '../img/logo.jpg';
+
+console.log('hello world');
+
+// const mainLogo = document.getElementById('company-logo');
+// mainLogo.src = companyLogo;
---
+++ webpack.config.js
@@ -0,0 +1,49 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+
+module.exports = {
+    mode: 'development',
+    entry: './src/index.js',
+    output: {
+        filename: 'bundle.js',
+        path: path.resolve(__dirname, 'build'),
+    },
+    devServer: {
+        static: './build',
+        open: true,
+        port: 8080
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: './index.html'
+        }),
+        new CleanWebpackPlugin()
+    ],
+    module: {
+        rules: [
+            {
+                test: /\.m?js$/,
+                exclude: /(node_modules|bower_components)/,
+                use: {
+                    loader: 'babel-loader',
+                    options: {
+                        presets: ['@babel/preset-env']
+                    }
+                }
+            },
+            {
+                test: /\.css$/i,
+                use: ["style-loader", "css-loader"],
+            },
+            {
+                test: /\.(png|jpe?g|gif)$/i,
+                use: [
+                    {
+                        loader: 'file-loader',
+                    },
+                ],
+            },
+        ]
+    }
+};
