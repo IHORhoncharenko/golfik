88fcae32aaaaa78fa6aa9cec94f97b57905e2f6e
Igor Goncharenko <gon4arenko.igor@gmail.com>
2022-04-10 10:05:07 +0300

    create generate html pages with webpack

--- webpack.config.js
+++ webpack.config.js
@@ -6,10 +6,43 @@ const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 const CssMinimizerPlugin = require("css-minimizer-webpack-plugin");
 const TerserPlugin = require("terser-webpack-plugin");
 
+const createHtmlPagesBlogWithWebpack = (fileName) => {
+    return new HtmlWebpackPlugin({
+        filename: `${fileName.toLowerCase()}.html`,
+        template: `./src/blog/${fileName.toLowerCase()}.html`,
+    });
+}
+
+const createHTMLPagesBlogArr = (pagesArray) => {
+    res = [];
+    for (let i = 0; i < pagesArray.length; i++) {
+        res.push(createHtmlPagesBlogWithWebpack(pagesArray[i]));
+    }
+    return res;
+}
+
+const htmlPagesBlog = createHTMLPagesBlogArr([
+    'blog',
+    'day-1',
+    'day-2',
+    'day-3',
+    'day-4',
+    'day-5',
+    'day-6',
+    'day-7',
+    'day-8',
+    'day-9',
+    'day-10',
+    'day-11',
+    'day-12',
+    'day-13'
+]);
+
 module.exports = {
     mode: 'development',
     entry: {
-        main: './src/index.js'
+        main: './src/index.js',
+        blog: './src/blog/script-blog.js'
     },
     output: {
         filename: '[name].[contenthash].js',
@@ -22,17 +55,18 @@ module.exports = {
     },
     plugins: [
         new HtmlWebpackPlugin({
+            filename: 'index.html',
             template: './src/index.html',
             // minify: {
             //     collapseWhitespace: true
             // }
         }),
+        ...htmlPagesBlog,
         new CleanWebpackPlugin(),
         new CopyPlugin({
             patterns: [
                 { from: path.resolve(__dirname, 'src/assets/favicon'), to: path.resolve(__dirname, 'build') },
                 { from: path.resolve(__dirname, 'src/assets/img'), to: path.resolve(__dirname, 'build') },
-                { from: path.resolve(__dirname, 'src/assets/blog'), to: path.resolve(__dirname, 'build') },
             ],
         }),
         new MiniCssExtractPlugin({
